f7d6a35647308b4b28a3620c97a4f0473a1e6e02
mohamadalabsi <absimoda1@gmail.com>
2025-03-31 14:04:14 +0200

    finishing Api gateway

---
+++ src/main/java/org/me/learning/apigatewaymicros/ApiGatewayMicroSApplication.java
@@ -0,0 +1,14 @@
+package org.me.learning.apigatewaymicros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ApiGatewayMicroSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayMicroSApplication.class, args);
+    }
+
+}
